c4bd2ef400a2252b87830212a1823427e78e8d7e
jubilanttree <treejubilant@gmail.com>
2017-02-06 22:39:33 +0530

    Add files via upload

---
+++ inc/header.inc.php
@@ -0,0 +1,4 @@
+<div class="page-top clearfix" ng-controller="headerController" >
+   <a href="#/dashboard" class="al-logo clearfix">Employee Dashboard</a>
+   <div class="questions-section">{{email}}</div>
+</div>
---
+++ inc/left.inc.php
@@ -0,0 +1,11 @@
+<aside class="al-sidebar" ng-controller="leftController">
+  <div class="slimScrollDiv">
+	 <ul class="al-sidebar-list">
+		<li class="al-sidebar-list-item selected" ng-class="{'selected': location == '/dashboard'}" >
+			<a class="al-sidebar-list-link" href="#/dashboard">
+				<i class="ion-android-home"></i><span>Dashboard</span>
+			</a>
+		</li>
+	 </ul>
+  </div>
+</aside>
